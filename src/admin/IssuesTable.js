f99537b53d2763d57a26f4ec6811c8079aa6252f
rohanraaz <cs20b042@smail.iitm.ac.in>
2023-08-08 14:53:10 +0530

    idk

--- src/admin/IssuesTable.js
+++ src/admin/IssuesTable.js
@@ -36,7 +36,6 @@ const IssuesTable = (data_table) => {
           </tr>
         </thead>
         <tbody>
-<<<<<<< HEAD
           {data.map((issue) => (
             (issue === null) ? null :
               (<tr key={issue.No}>
@@ -55,25 +54,6 @@ const IssuesTable = (data_table) => {
                 }
               </tr>)
           ))}
-=======
-            {data.map((issue) => (
-            <tr key={issue.No}>
-                <td style={{border:"2px solid black"}}>{issue.No}</td>
-                <td style={{border:"2px solid black"}}>{issue.Lab}</td>
-                <td style={{border:"2px solid black"}}>{issue.PcNo}</td>
-                <td style={{border:"2px solid black"}}>{issue.IssueType}</td>
-                <td style={{border:"2px solid black"}}>{issue.description}</td>
-                <td style={{border:"2px solid black"}}>{issue.IssueRaisedDate}</td>
-                <td style={{border:"2px solid black"}}>
-                <select className='pl-5 pr-5'>
-                  <option value="pending">pending</option>
-                  <option value="work in progess">work in progess</option>
-                  <option value="resolved">Resolved</option>
-                </select>
-                </td>
-            </tr>
-            ))}
->>>>>>> eb57199ea7f47c3eaee6ca7bd926613cce05d7d5
         </tbody>
       </table>
 
