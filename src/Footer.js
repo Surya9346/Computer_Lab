6ac9e66d05fefe767c8bab002173855e1a6b0602
rohanraaz <cs20b042@smail.iitm.ac.in>
2023-08-08 14:58:49 +0530

    done

--- src/Footer.js
+++ src/Footer.js
@@ -3,25 +3,15 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-<<<<<<< HEAD
-    <div className='bgfooter d-flex flex-row justify-content-center'>
-      <div className=''>
-        <p className='textclr mr-2'><a href='mailto:rishik4384@gmail.com;padmanabhunisurya9346@gmail.com;cs20b042@smail.iitm.ac.in'>Contact Us</a></p>
-=======
     <div className='bgfooter'>
       <div>
         <h2 className='textclr ml-1 pt-2' style={{textAlign:'left'}}>Contact Us</h2>
         <p className='textclr ml-4'>Name  : abcdef</p>
         <p className='textclr ml-4'>Ph.No : 9876543210</p>
         <p className='textclr ml-4'>Email : abcdef123@gmail.com</p>
->>>>>>> eb57199ea7f47c3eaee6ca7bd926613cce05d7d5
       </div>
       <div>
-<<<<<<< HEAD
-        <p className='textclr ml-2'><a href='https://forms.gle/3nKmmxaRVY2zGUyn7'>Feedback</a></p>
-=======
-        <h2 className='textclr ml-1 pb-2' style={{fontSize:'25px'}}>Feedback</h2>
->>>>>>> eb57199ea7f47c3eaee6ca7bd926613cce05d7d5
+        <h2 className='textclr ml-1 pb-2' style={{fontSize:'25px'}}><a href='https://forms.gle/Gj5nMBU738P5opCf6' target='_blank' rel='noreferrer'>Feedback</a></h2>
       </div>
     </div>
   )
