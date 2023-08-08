898bbca94a49878df03a0c41258620db41dc8a64
rohanraaz <cs20b042@smail.iitm.ac.in>
2023-08-08 10:45:42 +0530

    admin side table

--- src/admin/IssuesTable.js
+++ src/admin/IssuesTable.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 const IssuesTable = (data_table) => {
-  const data = [
-    { No: 1, Lab: 'Lab 1', PcNo: 13, IssueType: 'Hardware Issue', IssueRaisedDate: '20-07-2023', IssueResolvedDate: '24-07-2023', IssueSatus: 'Resolved'},
-    { No: 1, Lab: 'Lab 1', PcNo: 13, IssueType: 'Hardware Issue', IssueRaisedDate: '20-07-2023', IssueResolvedDate: '24-07-2023', IssueSatus: 'Resolved'},
-    { No: 1, Lab: 'Lab 1', PcNo: 13, IssueType: 'Hardware Issue', IssueRaisedDate: '20-07-2023', IssueResolvedDate: '24-07-2023', IssueSatus: 'Resolved'},
-    { No: 1, Lab: 'Lab 1', PcNo: 13, IssueType: 'Hardware Issue', IssueRaisedDate: '20-07-2023', IssueResolvedDate: '24-07-2023', IssueSatus: 'Resolved'},
-    { No: 1, Lab: 'Lab 1', PcNo: 13, IssueType: 'Hardware Issue', IssueRaisedDate: '20-07-2023', IssueResolvedDate: '24-07-2023', IssueSatus: 'Resolved'},
-  ];
+  const data = data_table.data_table;
   return (
     <div style={{ textAlign: 'center', paddingLeft: '120px', paddingTop: '30px' }}>
       <table style={{border:"2px solid black"}}>
@@ -17,6 +11,7 @@ const IssuesTable = (data_table) => {
               <th style={{border:"2px solid black"}} className='p-2'>Lab</th>
               <th style={{border:"2px solid black"}} className='p-2'>PC No</th>
               <th style={{border:"2px solid black"}} className='p-2'>Issue Type</th>
+              <th style={{border:"2px solid black"}} className='p-2'>Description</th>
               <th style={{border:"2px solid black"}} className='p-2'>Issue Raised Date</th>
               <th style={{border:"2px solid black"}} className='p-2'>Issue Resolved Date</th>
               <th style={{border:"2px solid black"}} className='p-2'>Issue Status</th>
@@ -29,6 +24,7 @@ const IssuesTable = (data_table) => {
                 <td style={{border:"2px solid black"}}>{issue.Lab}</td>
                 <td style={{border:"2px solid black"}}>{issue.PcNo}</td>
                 <td style={{border:"2px solid black"}}>{issue.IssueType}</td>
+                <td style={{border:"2px solid black"}}>{issue.description}</td>
                 <td style={{border:"2px solid black"}}>{issue.IssueRaisedDate}</td>
                 <td style={{border:"2px solid black"}}>{issue.IssueResolvedDate}</td>
                 <td style={{border:"2px solid black"}}>{issue.IssueSatus}</td>
